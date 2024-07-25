e01efd3582470bd71d2537648a62fe7afb668d20
César José Uriarte Chavesta <cesar.uriarte@unmsm.edu.pe>
2024-07-25 13:55:14 -0500

    Se eliminan animaciones

--- src/components/Motion/variants.ts
+++ src/components/Motion/variants.ts
@@ -3,13 +3,6 @@ export const fadeln = (direction: "left" | "right" | "up" | "down", delay: numbe
         hidden: {
             y: direction === "up" ? 40 : direction === "down" ? -40 : 0,
             x: direction === "left" ? 40 : direction === "right" ? -40 : 0,
-            opacity: 0.5,
-            transition: {
-                type: "tween",
-                duration: 1,
-                delay: delay,
-                ease: [0.42, 0, 0.58, 1]
-            }
         },
         show: {
             y: 0,
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,4 @@
 import IamSection from "./IamSection"
-import { motion } from "framer-motion"
-import { fadeln } from "../../components/Motion/variants"
 import Bar from "../../components/Bar"
 import About from "./About"
 import Experience from "./Experience"
@@ -10,37 +8,21 @@ const sections = ["Home", "About", "Experience"]
 
 function Home() {
   return (
-    <div className="flex justify-center items-center relative w-ful md:w-[calc(100%-100px)] !scroll-smooth">
+    <div className="flex justify-center items-center relative w-full md:w-[calc(100%-100px)]">
       <div>
         <div id="Home">
-          <motion.div
-            variants={fadeln("left", 0.2)}
-            initial="hidden"
-            whileInView={"show"}
-            viewport={{ once: false, amount: 0.7 }}
-          >
+          <div>
             <IamSection className="min-h-[calc(100vh-64px)]" />
-          </motion.div>
+          </div>
         </div>
         <div id="About">
-          <motion.div
-            variants={fadeln("right", 0.2)}
-            initial="hidden"
-            whileInView={"show"}
-            viewport={{ once: false, amount: 0.7 }}
-          >
+          <div>
             <About />
-          </motion.div>
+          </div>
         </div>
-        <motion.div
-          variants={fadeln("left", 0.2)}
-          initial="hidden"
-          whileInView={"show"}
-          viewport={{ once: false, amount: 0.7 }}
-          id="Experience"
-        >
+        <div id="Experience">
           <Experience />
-        </motion.div>
+        </div>
       </div>
       <Bar sectionsId={sections} className="h-[calc(100vh-100px)]" />
     </div>
